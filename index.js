5663df8ee663e4611726fd53d400968b00610d36
Krishna-Potter <krishna.vebbox@gmail.com>
2022-02-22 12:00:58 +0530

    add tasks in todo

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const tasks = [];
 const addTask = () => {
   let storeTask = document.getElementById("getTaskTextbox").value;
   tasks.push(storeTask);
-
   for (let task of tasks) {
     let creatTask = document.createElement("p");
     body.append(creatTask);
+    tasks.pop(task);
     creatTask.innerHTML = task;
-    console.log(task);
   }
 };
